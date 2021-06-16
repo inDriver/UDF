d50a3d23c7d1d252037de52761cc4de536231041
Anton Goncharov <anton.goncharov@mybuddy.ai>
2021-06-16 16:09:25 +0300

    add Example project

---
+++ Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Anton Goncharov on 16.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
