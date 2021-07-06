a2d05dd2d9e77c2d4924f41fc0a1892def2cf31c
trykovyura <yuri.trykov@indriver.com>
2021-07-06 18:05:10 +0300

    UDF CI

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import UDFTests
-
-var tests = [XCTestCaseEntry]()
-tests += UDFTests.allTests()
-XCTMain(tests)
---
+++ UDFTests/UDFTests.swift
@@ -0,0 +1,33 @@
+//
+//  UDFTests.swift
+//  UDFTests
+//
+//  Created by IURII TRYKOV on 06.07.2021.
+//
+
+import XCTest
+@testable import UDF
+
+class UDFTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
