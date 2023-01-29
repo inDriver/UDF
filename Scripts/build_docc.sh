3a878d7e1b0401db176600af92998b8d577252a3
alexey1312 <alexey1312ru@gmail.com>
2023-01-29 19:15:01 +0600

    Add workflow for deploy docc

---
+++ Scripts/build_docc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+xcodebuild docbuild \
+    -scheme UDF \
+    -derivedDataPath /tmp/docbuild/udf \
+    -destination 'generic/platform=iOS'
+
+$(xcrun --find docc) process-archive \
+            transform-for-static-hosting /tmp/docbuild/udf/Build/Products/Debug-iphoneos/UDF.doccarchive \
+            --output-path docc
+
+echo "<script>window.location.href += \"documentation/udf\"</script>" > docc/index.html;
